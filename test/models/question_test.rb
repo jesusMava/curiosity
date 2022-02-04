4f7249e41ca2a6ac12352b32a441e13738b47627
jesusMava <jesusmacedo7@gmail.com>
2022-02-04 11:24:55 -0600

    Test question model

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -3,4 +3,15 @@
 require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
+  test 'should be a correct answer' do
+    question = build(:question)
+
+    assert question.correct?
+  end
+
+  test 'should be a incorrect answer' do
+    question = build(:question, answer: false)
+
+    assert_not question.correct?
+  end
 end
