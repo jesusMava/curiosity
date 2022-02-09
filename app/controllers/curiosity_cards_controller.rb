e5b575a7c2237a2d69034933d384a2acb19cd3a4
jesusMava <jesusmacedo7@gmail.com>
2022-02-09 17:53:46 -0600

    Feature: Add kaminari with bulma theme

--- app/controllers/curiosity_cards_controller.rb
+++ app/controllers/curiosity_cards_controller.rb
@@ -5,7 +5,7 @@ class CuriosityCardsController < ApplicationController
   before_action :set_curiosity, only: %i[show edit update destroy]
 
   def index
-    @curiosities = policy_scope(CuriosityCard).search(params[:query])
+    @curiosities = policy_scope(CuriosityCard).search(params[:query]).page(params[:page]).per(9)
   end
 
   def show; end
