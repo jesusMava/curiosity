0ab2b8d51dd411b6b3002fb8e27f4ef790277acd
jesusMava <jesusmacedo7@gmail.com>
2022-02-11 00:51:59 -0600

    Test: System test for game

--- test/system/games_test.rb
+++ test/system/games_test.rb
@@ -3,39 +3,60 @@
 require 'application_system_test_case'
 
 class GamesTest < ApplicationSystemTestCase
-  setup do
-    @game = games(:one)
+  def user
+    @user ||= create(:user)
   end
 
-  test 'visiting the index' do
-    visit games_url
-    assert_selector 'h1', text: 'Games'
+  setup do
+    user
+  end
+
+  test 'visiting the index game' do
+    sign_in user
+
+    visit games_path
+
+    assert_selector 'h1', text: 'Curiosity Game'
+  end
+
+  test 'Top 10 players' do
+    sign_in user
+
+    visit games_path
+
+    assert_selector 'h2', text: 'Top 10 players'
   end
 
   test 'should create game' do
-    visit games_url
-    click_on 'New game'
+    sign_in user
 
+    visit games_path
+    click_on 'Start a new game'
     click_on 'Create Game'
 
-    assert_text 'Game was successfully created'
-    click_on 'Back'
+    assert_text 'Game was successfully created.'
+    assert_selector 'h2', text: 'Total score: 0'
   end
 
-  test 'should update Game' do
-    visit game_url(@game)
-    click_on 'Edit this game', match: :first
+  test 'should play a game' do
+    sign_in user
+    game = create(:game, user: user)
+    create_list(:question, 5, game: game, answer: nil)
 
-    click_on 'Update Game'
+    visit game_path(game)
 
-    assert_text 'Game was successfully updated'
-    click_on 'Back'
-  end
-
-  test 'should destroy Game' do
-    visit game_url(@game)
-    click_on 'Destroy this game', match: :first
-
-    assert_text 'Game was successfully destroyed'
+    assert_selector 'h2', text: 'Total score: 0'
+    click_on 'Real'
+    assert_selector 'h2', text: 'Total score: 20'
+    click_on 'Real'
+    assert_selector 'h2', text: 'Total score: 40'
+    click_on 'Fake'
+    assert_selector 'h2', text: 'Total score: 40'
+    click_on 'Fake'
+    assert_selector 'h2', text: 'Total score: 40'
+    click_on 'Real'
+    assert_selector 'h2', text: 'Total score: 60'
+    assert_selector 'h1', text: 'Game over!!'
+    assert_link('Play again')
   end
 end
